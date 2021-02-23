7afebe4aa66e462954b08c7f0cb9be576881b667
akash5100 <shivamvermashs@gmail.com>
2021-02-23 10:39:51 +0530

    finally done with creating node

--- Btree/btree.cpp
+++ Btree/btree.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+class btree_node
+{
+    int *data;
+    int t;
+    btree_node *ptr;
+};
+
+class BTree
+{
+    btree_node *root;
+    int t;
+public:
+    //constructor
+    BTree(int T)
+    {
+        root = nullptr;
+        t = T;
+    }
+};
+
 int main(void)
 {
-    //this is test commit.
+    BTree t(3);
 }
