19896b4989d40228955d7622bfc9c27909f7dc19
akash5100 <shivamvermashs@gmail.com>
2021-02-23 17:43:04 +0530

    fish shell configs

--- Btree/btree.cpp
+++ Btree/btree.cpp
@@ -6,7 +6,14 @@ class btree_node
 {
     int *data;
     int t;
-    btree_node *ptr;
+    btree_node **ptr;
+public:
+    //constructor
+    btree_node(int T)
+    {
+        data = new int[T-1];
+        ptr = new btree_node *[T];
+    }
 };
 
 class BTree
